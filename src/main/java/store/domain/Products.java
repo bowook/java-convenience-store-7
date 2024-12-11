85506fa0013e1b7657a8b7858442d123451be2f0
bowook <mbosx316@naver.com>
2024-12-11 19:12:40 +0900

    feat(Products) : 재고 관리 도메인 추가

--- src/main/java/store/domain/Products.java
+++ src/main/java/store/domain/Products.java
@@ -1,7 +1,10 @@
 package store.domain;
 
+import camp.nextstep.edu.missionutils.DateTimes;
 import java.util.ArrayList;
 import java.util.List;
+import store.exception.CustomException;
+import store.exception.ErrorMessage;
 
 public class Products {
     private final List<PromotionProduct> promotionProducts = new ArrayList<>();
@@ -23,7 +26,7 @@ public class Products {
             }
         }
     }
-    
+
     private boolean findSameGeneralProduct(PromotionProduct promotionProduct) {
         for (GeneralProduct generalProduct : generalProducts) {
             if (promotionProduct.getName().equals(generalProduct.getName())) {
@@ -31,5 +34,118 @@ public class Products {
             }
         }
         return false;
+    }
+
+    public void checkQuantity(String productName, int inputQuantity) {
+        int quantity = 0;
+        quantity += findPromotionQuantity(productName);
+        quantity += findGeneralQuantity(productName);
+        if (inputQuantity > quantity) {
+            throw CustomException.from(ErrorMessage.OVER_COUNT_ERROR);
+        }
+    }
+
+    public void subtractOneMore(String productName) {
+        PromotionProduct promotionProduct = findPromotionProduct(productName);
+        promotionProduct.subtractQuantity(1);
+    }
+
+    public boolean checkOneMorePromotion(String productName, int quantity, User user) {
+        PromotionProduct promotionProduct = findPromotionProduct(productName);
+        if (promotionProduct.remainPromotionBuy(quantity)) {
+            promotionProduct.subtractQuantity(quantity);
+            user.addPromotionPurchase(new PromotionPurchase(promotionProduct, quantity));
+            return true;
+        }
+        promotionProduct.subtractQuantity(quantity);
+        user.addPromotionPurchase(new PromotionPurchase(promotionProduct, quantity));
+        return false;
+    }
+
+    public void subtractGeneralProduct(String productName, int quantity) {
+        GeneralProduct generalProduct = findGeneralProduct(productName);
+        generalProduct.subtractQuantity(quantity);
+    }
+
+    public int subtractAllQuantity(String productName, int quantity, User user) {
+        PromotionProduct promotionProduct = findPromotionProduct(productName);
+        int promotionProductQuantity = promotionProduct.getQuantity();
+        promotionProduct.subtractQuantity(promotionProductQuantity);
+        user.addPromotionPurchase(new PromotionPurchase(promotionProduct, promotionProductQuantity));
+        return quantity - promotionProductQuantity;
+    }
+
+    public boolean useOnlyPromotion(String productName, int quantity) {
+        for (PromotionProduct promotionProduct : promotionProducts) {
+            if (promotionProduct.getName().equals(productName) && promotionProduct.getQuantity() >= quantity) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isPromotionProduct(String productName) {
+        for (PromotionProduct promotionProduct : promotionProducts) {
+            if (promotionProduct.getName().equals(productName) && promotionProduct.checkDate(DateTimes.now())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int findGeneralQuantity(String productName) {
+        for (GeneralProduct generalProduct : generalProducts) {
+            if (generalProduct.getName().equals(productName)) {
+                return generalProduct.getQuantity();
+            }
+        }
+        return 0;
+    }
+
+    private int findPromotionQuantity(String productName) {
+        for (PromotionProduct promotionProduct : promotionProducts) {
+            if (promotionProduct.getName().equals(productName)) {
+                return promotionProduct.getQuantity();
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        promotionToString(stringBuilder);
+        generalToString(stringBuilder);
+        return stringBuilder.toString();
+    }
+
+    private void promotionToString(StringBuilder stringBuilder) {
+        for (PromotionProduct promotionProduct : promotionProducts) {
+            String product = String.format("- %s %,d원 %d개 %s%n", promotionProduct.getName(),
+                    promotionProduct.getPrice(), promotionProduct.getQuantity(), promotionProduct.getPromotionName());
+            stringBuilder.append(product);
+        }
+    }
+
+    private void generalToString(StringBuilder stringBuilder) {
+        for (GeneralProduct pr : generalProducts) {
+            if (pr.getQuantity() == 0) {
+                String product = String.format("- %s %,d원 재고 없음%n", pr.getName(), pr.getPrice());
+                stringBuilder.append(product);
+                continue;
+            }
+            String product = String.format("- %s %,d원 %d개%n", pr.getName(), pr.getPrice(), pr.getQuantity());
+            stringBuilder.append(product);
+        }
+    }
+
+    private PromotionProduct findPromotionProduct(String name) {
+        return promotionProducts.stream().filter(promotionProduct -> promotionProduct.getName().equals(name))
+                .findFirst().orElseThrow();
+    }
+
+    public GeneralProduct findGeneralProduct(String name) {
+        return generalProducts.stream().filter(generalProduct -> generalProduct.getName().equals(name))
+                .findFirst().orElseThrow();
     }
 }
