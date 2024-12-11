726f31b59c3ccfa815e60a3669cc0a6a52eda809
bowook <mbosx316@naver.com>
2024-12-11 19:39:29 +0900

    refactor(InputView) : 입력 기능 개선

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -8,8 +8,8 @@ import store.utils.Parser;
 
 public class InputView {
     private final static String INPUT_PURCHASE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
-    private final static String INPUT_ONE_MORE = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)";
-    private final static String INPUT_NO_DISCOUNT = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)";
+    private final static String INPUT_ONE_MORE = "현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n";
+    private final static String INPUT_NO_DISCOUNT = "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n";
     private final static String INPUT_MEMBERSHIP = "멤버십 할인을 받으시겠습니까? (Y/N)";
     private final static String INPUT_RETRY = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
