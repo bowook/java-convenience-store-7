68b40dd6f66c65f286ccf663b81e29e652eb8498
bowook <mbosx316@naver.com>
2024-12-11 19:13:39 +0900

    feat(PromotionPurchase) : 프로모션 구매 도메인 추가

--- src/main/java/store/domain/PromotionPurchase.java
+++ src/main/java/store/domain/PromotionPurchase.java
@@ -1,2 +1,42 @@
-package store.domain;public class Purchase {
+package store.domain;
+
+public class PromotionPurchase {
+    private final PromotionProduct promotionProduct;
+    private int quantity;
+
+    public PromotionPurchase(final PromotionProduct promotionProduct, final int quantity) {
+        this.promotionProduct = promotionProduct;
+        this.quantity = quantity;
+    }
+
+    public int calculateGet() {
+        return promotionProduct.calculateGet(quantity);
+    }
+
+    public int calculatePromotionDiscount() {
+        return calculateGet() * promotionProduct.getPrice();
+    }
+
+    public String getName() {
+        return promotionProduct.getName();
+    }
+
+    public void addQuantityOneMore(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTotalPrice() {
+        return promotionProduct.getPrice() * quantity;
+    }
+
+    @Override
+    public String toString() {
+        String priceFormat = String.format("%,d", promotionProduct.getPrice() * quantity);
+        return promotionProduct.getName() + "\t\t" + quantity + "\t\t" + priceFormat
+                + "\n";
+    }
 }
