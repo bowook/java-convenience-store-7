40f3080c824c03628102402b62896b9624b7befc
bowook <mbosx316@naver.com>
2024-12-11 19:12:21 +0900

    feat(GeneralPurchase) : 일반상품구매 도메인 추가

--- src/main/java/store/domain/GeneralPurchase.java
+++ src/main/java/store/domain/GeneralPurchase.java
@@ -1,2 +1,30 @@
-package store.domain;public class GeneralPurchase {
+package store.domain;
+
+public class GeneralPurchase {
+    private final GeneralProduct generalProduct;
+    private int quantity;
+
+    public GeneralPurchase(final GeneralProduct generalProduct, final int quantity) {
+        this.quantity = quantity;
+        this.generalProduct = generalProduct;
+    }
+
+    public String getName() {
+        return generalProduct.getName();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTotalPrice() {
+        return generalProduct.getPrice() * quantity;
+    }
+
+    @Override
+    public String toString() {
+        String priceFormat = String.format("%,d", generalProduct.getPrice() * quantity);
+        return generalProduct.getName() + "\t\t" + quantity + "\t\t" + priceFormat
+                + "\n";
+    }
 }
