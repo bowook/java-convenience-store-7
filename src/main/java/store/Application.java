c34208d14b98f0f837b14f0f172aa5e39462ce0c
bowook <mbosx316@naver.com>
2024-12-11 19:11:12 +0900

    feat(main) : 메인 생성

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,10 +1,13 @@
 package store;
 
+import store.controller.StoreController;
 import store.service.StoreService;
+import store.view.InputView;
+import store.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        StoreService service = new StoreService();
-        
+        StoreController storeController = new StoreController(new InputView(), new OutputView(), new StoreService());
+        storeController.start();
     }
 }
